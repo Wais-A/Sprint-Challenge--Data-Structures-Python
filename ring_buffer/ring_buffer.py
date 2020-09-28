220f8cfdeafda50ec2aca770e50649aba5fc3e30
Wais-A <wais.almakaleh@gmail.com>
2020-09-27 22:29:25 -0400

    buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        
+    old = 0
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity:
+            self.data.remove(self.data[self.old])
+            self.data.insert(self.old, item)
+            if self.old + 1 == 5:
+                self.old = 0
+            else:
+                self.old += 1
+        elif len(self.data) == 0:
+            self.data.insert(0, item)
+        else:
+            self.data.append(item)
 
     def get(self):
-        pass
+        return self.data
